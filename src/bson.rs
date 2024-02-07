e73b0a737542cb04f44664dd07a8482c624d818b
KantaiKessen <kdliu02@gmail.com>
2024-02-07 09:48:42 -0800

    bson: rewrote encapsulate fn
    The encapsulate function was rewritten in order to have the correct parameters and return type.
    
    Signed-off-by: Kevin Liu <kevin.liu@csu.fullerton.edu>

--- src/bson.rs
+++ src/bson.rs
@@ -5,41 +5,35 @@
 // work based on the protocol version.
 use bson::{bson, Bson};
 use serde::{Deserialize, Serialize};
+use std::io::{Error, ErrorKind};
 
 #[derive(Serialize, Deserialize)]
-struct ACCV1Struct {
+struct AccVersion1 {
     v: u32,
     //other stuff
     d: Vec<u8>,
 }
-impl ACCV1Struct {
-    fn new(d: &Vec<u8>) -> ACCV1Struct {
-        ACCV1Struct { v: 1, d: d.clone() }
+impl AccVersion1 {
+    fn new(d: &Vec<u8>) -> AccVersion1 {
+        AccVersion1 { v: 1, d: d.clone() }
     }
 }
 
-fn encapsulate(pkt: Vec<u8>) -> Vec<u8> {
-    let acc_struct: ACCV1Struct = ACCV1Struct::new(&pkt);
-    let bson_vec = bson::to_vec(&acc_struct).unwrap();
-    bson_vec
+enum PacketVersion {
+    V1(AccVersion1),
 }
 
-fn reveal(bson_doc: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
+fn encapsulate(acc_struct: PacketVersion) -> Result<Vec<u8>, std::io::Error> {
+    let pkt = match acc_struct {
+        PacketVersion::V1(pkt) => pkt,
+        _ => return Err(Error::from(ErrorKind::Unsupported)),
+    };
+    let bson_vec = bson::to_vec(&pkt).unwrap();
+    Ok(bson_vec)
+}
+
+fn reveal(bson_doc: Vec<u8>) -> Result<PacketVersion, std::io::Error> {
+    //let bson_data: Bson = bson::from_slice(&bson_doc[..]).unwrap();
+    //if()
     unimplemented!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn encapsulate_test() {
-        let pkt: Vec<u8> = Vec::from(b"Hello Darkness my old friend");
-        println!("pkt is {:x?}", pkt);
-        let acc_struct: ACCV1Struct = ACCV1Struct::new(&pkt);
-        println!("acc ver is {}", acc_struct.v);
-        println!("acc data is {:x?}", acc_struct.d);
-        let encapsulated = encapsulate(pkt);
-        println!("encapsulated is {:x?}", encapsulated);
-    }
 }
